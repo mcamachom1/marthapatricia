ad91a29c5ea0539d5b1043eedaf726979c859239
mcamachom1 <mcamachom1@dian.gov.co>
2024-05-16 09:55:44 -0500

    Update CartItem.cs

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -16,7 +16,7 @@ namespace PartsUnlimited.Models
         public string CartId { get; set; }
         public int ProductId { get; set; }
         public int Count { get; set; }
-        public integer345r
+        public integer345r#
 
         [DataType(DataType.DateTime)]
         public DateTime DateCreated { get; set; }
